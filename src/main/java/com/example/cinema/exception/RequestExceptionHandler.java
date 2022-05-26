b7b361a412b364ebf4f3616a34ae19a0c18a2244
puscik21 <grzesiekpuscizna@gmail.com>
2022-05-26 15:55:16 +0200

    Basic logging config

--- src/main/java/com/example/cinema/exception/RequestExceptionHandler.java
+++ src/main/java/com/example/cinema/exception/RequestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.cinema.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -10,18 +11,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @ControllerAdvice
 public class RequestExceptionHandler {
 
     @ExceptionHandler(value = RequestException.class)
     public ResponseEntity<ExceptionResult> handleRequestException(RequestException e) {
         ExceptionResult result = new ExceptionResult(e.getMessage(), HttpStatus.BAD_REQUEST);
+        log.error("{}, status: {}, stackTrace: ", e.getMessage(), HttpStatus.BAD_REQUEST, e);
         return ResponseEntity.badRequest().body(result);
     }
 
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ExceptionResult> handleRequestException(Exception e) {
         ExceptionResult result = new ExceptionResult(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error("{}, status: {}, stackTrace: ", e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, e);
         return ResponseEntity.badRequest().body(result);
     }
 
@@ -31,6 +35,7 @@ public class RequestExceptionHandler {
         for (ObjectError error : e.getBindingResult().getAllErrors()) {
             details.add(error.getDefaultMessage());
         }
+        log.error("{}, status: {}, stackTrace: ", details, HttpStatus.BAD_REQUEST, e);
         MethodArgumentNotValidExceptionResult result = new MethodArgumentNotValidExceptionResult(e.getMessage(), HttpStatus.PRECONDITION_FAILED, details);
         return ResponseEntity.badRequest().body(result);
     }
