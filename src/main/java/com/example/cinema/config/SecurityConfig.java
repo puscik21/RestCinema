38b8ad5a6630cc38c1ce5f9758ee3084e45dd8de
puscik21 <grzesiekpuscizna@gmail.com>
2022-06-05 13:29:13 +0200

    DELETE methods need user role 'ADMIN'

--- src/main/java/com/example/cinema/config/SecurityConfig.java
+++ src/main/java/com/example/cinema/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.cinema.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,10 +26,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic().and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.DELETE).hasRole("ADMIN")
                 .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+                .and().csrf().disable();
     }
 
     @Bean
