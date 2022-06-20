2ff1b857dbaebd4138b3d517454974385d5bd3a7
puscik21 <grzesiekpuscizna@gmail.com>
2022-06-20 23:02:21 +0200

    Fixed errors with testing after security changes

--- src/main/resources/test/testData.sql
+++ src/main/resources/test/testData.sql
@@ -20,4 +20,4 @@ insert into movie (id, name) values (1, 'Avatar');
 insert into spectacle (id, movie_id, auditorium_id, date_time) values (1, 1, 1, DATEADD(second, DATEDIFF(second, '19700101', NOW()), '19700101'));
 
 -- spectator
-insert into spectator (id, name, email, phone_number) values (1, 'Jan Kowalski', 'jankowalski@mail.com', '123456789');
+insert into spectator (id, name, email, phone_number, password, roles) values (1, 'Jan Kowalski', 'jankowalski@mail.com', '123456789', 'admin', '');
